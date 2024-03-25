c9dfdb8cb8af3076cabb74a98f6aac690992ae99
JeongYeonSeung <asas4654@daum.net>
2024-03-25 14:16:23 +0900

    feat : 이메일 중복 검사, 이메일 인증, 회원가입 로직 구현

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UserService } from './user.service';
+import { VerifyEmailDto } from './dto/verify-email.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post('check-email-duplication')
+  async checkEmailDuplication(@Body() body): Promise<boolean> {
+    const email = body.email;
+    const check = await this.userService.checkForDuplicateEmail(email);
+    if (check) {
+      console.log('중복 검사 완료!');
+      return true;
+    }
+  }
+
+  @Post('send-verify-email')
+  async sendVerifyEmail(@Body() body): Promise<void> {
+    const email = body.email;
+    await this.userService.sendVerification(email);
+  }
+
+  @Post('verify-email')
+  async verifyEmail(@Body() dto: VerifyEmailDto): Promise<boolean> {
+    const email = dto.email;
+    const token = dto.token;
+    const check = await this.userService.verifyToken(email, token);
+    if (check) {
+      console.log('이메일 인증 완료!');
+      return true;
+    }
+  }
+
+  @Post('register')
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    const { email, nickname, password } = dto;
+    await this.userService.createUser(email, nickname, password);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,128 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/entities/user.entity';
+import { EmailService } from 'src/services/email.service';
+import { Repository } from 'typeorm';
+import { hash } from 'bcrypt';
+import { EmailTokenEntity } from 'src/entities/email-token.entity';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+    @InjectRepository(EmailTokenEntity)
+    private readonly emailTokenRepository: Repository<EmailTokenEntity>,
+    private readonly emailService: EmailService,
+  ) {}
+
+  // email 중복 검사
+  async checkForDuplicateEmail(email: string) {
+    const isEmailValid = await this.isEmailValid(email);
+
+    if (!isEmailValid) {
+      throw new BadRequestException('올바른 이메일 형식이 아닙니다.');
+    }
+    const exitedUser = await this.userRepository.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (exitedUser) {
+      throw new BadRequestException('이미 가입된 이메일입니다.');
+    }
+
+    return true;
+  }
+
+  // 입력한 email로 인증 코드 발송하고, DB에 인증 코드 저장
+  async sendVerification(email: string) {
+    const verifyToken = await this.generateRandomToken();
+
+    const existedEmailToken = await this.emailTokenRepository.findOne({
+      where: { email },
+    });
+
+    if (existedEmailToken) {
+      existedEmailToken.token = verifyToken;
+      existedEmailToken.isVerified = false;
+      await this.emailTokenRepository.save(existedEmailToken);
+    } else {
+      const emailToken = new EmailTokenEntity();
+      emailToken.email = email;
+      emailToken.token = verifyToken;
+      emailToken.isVerified = false;
+      await this.emailTokenRepository.save(emailToken);
+    }
+
+    await this.emailService.sendVerificationToEmail(email, verifyToken);
+  }
+
+  // DB내의 인증 코드와 비교하여 검증
+  async verifyToken(email: string, userInputToken: string): Promise<boolean> {
+    const emailToken = await this.emailTokenRepository.findOne({
+      where: { email, token: userInputToken },
+    });
+
+    if (!emailToken) {
+      throw new BadRequestException('인증 코드가 일치하지 않습니다.');
+    } else {
+      emailToken.isVerified = true;
+      await this.emailTokenRepository.save(emailToken);
+    }
+
+    return true;
+  }
+
+  // 회원 가입 로직
+  async createUser(email: string, nickname: string, password: string) {
+    const isEmailVerified = await this.emailTokenRepository.findOne({
+      where: { email },
+    });
+
+    const isPasswordValid = await this.isPasswordValid(password);
+
+    if (!isEmailVerified || !isEmailVerified.isVerified) {
+      throw new BadRequestException('이메일 인증이 필요합니다.');
+    }
+
+    if (password.length < 8) {
+      throw new BadRequestException('비밀번호는 8자 이상이어야 합니다.');
+    }
+
+    if (!isPasswordValid) {
+      throw new BadRequestException('비밀번호는 특수문자를 포함해야 합니다.');
+    }
+
+    const hashedPassword = await hash(password, 10);
+    const user = await this.userRepository.save({
+      email: email,
+      nickname: nickname,
+      password: hashedPassword,
+    });
+
+    return user;
+  }
+
+  // 6자리 랜덤한 토큰 생성
+  private async generateRandomToken(): Promise<string> {
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let token = '';
+    for (let i = 0; i < 6; i++) {
+      token += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return token;
+  }
+  // 이메일 형식 검증
+  private async isEmailValid(email: string): Promise<boolean> {
+    const emailRegex = /^[^\s@]+@naver\.com$/;
+    return emailRegex.test(email);
+  }
+  // 비밀번호 형식 검증
+  private async isPasswordValid(password: string): Promise<boolean> {
+    const specialCharacters = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    return specialCharacters.test(password);
+  }
+}
