c8a48522eca3a6518c5fc702dcb0abe46240a168
JeongYeonSeung <asas4654@daum.net>
2024-03-25 14:16:23 +0900

    feat : 이메일 중복 검사, 이메일 인증, 회원가입 로직 구현

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/entities/user.entity';
+import { UserEntity } from 'src/entities/user.entity';
+import { EmailService } from 'src/services/email.service';
+import { EmailTokenEntity } from 'src/entities/email-token.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([UserEntity, EmailTokenEntity])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, EmailService],
 })
 export class UserModule {}
