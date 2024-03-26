8434b06110f742437456ff40b58c763919b3c678
JeongYeonSeung <asas4654@daum.net>
2024-03-26 17:41:18 +0900

    feat : typeorm 테스트를 위한 entity

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { CommonEntity } from './common.entity';
+
+@Entity('User')
+export class UserEntity extends CommonEntity {
+  @PrimaryGeneratedColumn({ name: 'user_id' })
+  userId: number;
+
+  @Column('varchar', { name: 'email', unique: true, nullable: false })
+  email: string;
+
+  @Column('varchar', { name: 'nickname', unique: true, nullable: false })
+  nickname: string;
+
+  @Column('varchar', { name: 'password', unique: false, nullable: false })
+  password: string;
+}
