24a7316ff02969e96cff5c483e85595024014bef
JeongYeonSeung <asas4654@daum.net>
2024-04-09 16:05:45 +0900

    cicd test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '배포 완료입니다!!! + DB도 연결됨!';
+    return 'ci/cd 설정도 완료';
   }
 }
